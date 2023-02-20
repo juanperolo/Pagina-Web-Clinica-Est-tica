e8dfb5f3c755de4427ca439f293ef7afee541d85
juanperolo <proyectos.ra.la@gmail.com>
2023-02-20 10:55:23 +0000

    commit 3

--- components/header.js
+++ components/header.js
@@ -5,16 +5,18 @@ class Header extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-          <header>
-            <nav>
-                <ul>
-                    <li><a href="main.html">Inicio</a></li>
-                    <li><a href="tratamientos.html">Tratamientos</a></li>
-                    <li><a href="nosotros.html">Nosotros</a></li>
-                    <li><a href="contacto.html">Contacto</a></li>
-                </ul>
-            </nav>
-        </header>
+          <div class="page-header">
+            <header>
+              <nav>
+                  <ul>
+                      <li><a href="main.html">Inicio</a></li>
+                      <li><a href="tratamientos.html">Tratamientos</a></li>
+                      <li><a href="nosotros.html">Nosotros</a></li>
+                      <li><a href="contacto.html">Contacto</a></li>
+                  </ul>
+              </nav>
+          </header>
+        </div>
         `;
       }
   }
